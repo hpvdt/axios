075267aebf0695fa546a4db0b767460f380b8f17
Savo <56771837+savob@users.noreply.github.com>
2022-04-01 02:28:05 -0400

    Uploaded tested CO2 code
    Tested and verified CO2 code for Axios

--- axios_rebreather/axios_rebreather.ino
+++ axios_rebreather/axios_rebreather.ino
@@ -21,6 +21,7 @@
     1 - D9 - Digit 3
     2 - D10 - Digit 4
 */
+
 #include "display.h"
 
 // Oxygen related pins
@@ -31,9 +32,15 @@ const byte oxygenCalibration[] = {A4, A3, A2};
 // CO2 related pin and variables
 const byte CO2Sensor = 2; // CO2 sensor pin
 const int CO2Span = 2000;                     // Span in ppm of CO2 of sensor
-const long CO2WarmUp = 240000;                // CO2 sensor warmup time in ms (datasheet says 3 minutes)
+const long CO2WarmUp = 180000;                // CO2 sensor warmup time in ms (datasheet says 3 minutes)
 volatile unsigned long CO2Level = 400;        // 400ish is ambient so good default
 volatile unsigned long lastRising, lastFalling;
+
+// Screen numbers
+const byte CO2Screen = 0;
+const byte O2Screen[] = {3, 2, 1};
+
+//#define TESTMODE // Uncomment for just showing random/test data on displays
 
 void setup() {
 
@@ -42,26 +49,33 @@ void setup() {
   // Set up CO2 sensor routine to run on PWM changes
   pinMode(CO2Sensor, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(CO2Sensor), CO2Interrupt, CHANGE);
-  
+
   //Serial.begin(9600);
 }
 
 
 void loop() {
-  // Read oxygen levels, display them appropriately
-  //displayFloat(oxygenRead(0), 1, 3);
-  //displayFloat(oxygenRead(1), 2, 3);
-  //displayFloat(oxygenRead(2), 3, 3);
 
+#ifdef TESTMODE
+  // Show random numbers on the middle two displays
+  displayInt(random(10000), O2Screen[random(2) + 1]);
+  displayFloat(millis() / 1000.0, O2Screen[0], 1);  // Time running on top display
+
+  displayInt(digitalRead(CO2Sensor), CO2Screen);  // Show state of sensor
+#else
+
+  // Read oxygen levels, display them appropriately (dashes if over 100%)
+  for (byte i = 0; i < 3; i++) {
+    if (oxygenRead(i) < 100.0) displayFloat(oxygenRead(i), O2Screen[i], 2);
+    else displayDashes(O2Screen[i]);
+  }
+
+  
   // CO2, shows value or countdown to when the sensor's ready if too early
-  
-  if (millis() < CO2WarmUp) displayFloat((CO2WarmUp - millis())/1000.0, 0, 1); // Warmup countdown (decimal indicates not CO2)
-  else displayInt(CO2Level, 0);               // Sensor should be operational
 
-  //if (millis() < CO2WarmUp) displayDashes(0);
-  
-  displayInt(random(10000), random(2) + 1);
-  displayFloat(millis()/1000.0,3,1); // Time running
+  if (millis() < CO2WarmUp) displayFloat((CO2WarmUp - millis()) / 1000.0, CO2Screen, 1); // Warmup countdown (decimal indicates not CO2)
+  else displayInt(CO2Level, CO2Screen);               // Sensor should be operational
+#endif
 
   delay(25);
 }
@@ -70,24 +84,24 @@ void CO2Interrupt () {
   // Based on time high and low for a PWM cycle, in milliseconds (total period ~1004ms)
 
   // NOTE THE PWM IS INVERTED BY THE LEVEL SHIFTER, HENCE THE SEEMINGLY CONTRADICTORY PIN STATES
-  
+
   unsigned long currentTime = millis();
-  
+
   if (digitalRead(CO2Sensor) == HIGH) {
-    lastFalling = currentTime; // Mark falling edge ofg PWM
+    lastFalling = currentTime; // Mark falling edge of PWM
   }
   else {
     // Rising edge, start of a new pulse
 
     // Calculate CO2 level from last pulse
     // CO2 = ppm span * (Th - 2ms) / (Th + Tl - 4ms)
-    
+
     unsigned long timeHigh = lastFalling - lastRising;
     unsigned long timeLow = currentTime - lastFalling;
-   
+
     CO2Level = CO2Span * (timeHigh - 2);
     CO2Level = CO2Level / (timeHigh + timeLow - 4);
-    
+
     lastRising = currentTime; // Update rising time
   }
 }
@@ -100,7 +114,7 @@ float oxygenRead(byte sensor) {
 
   // Math
   float b = cal / 21.0;
-  float m = (1010 - cal) / 79.0; // This is the gradient (slope) of the function.
+  float m = (1010.0 - cal) / 79.0; // This is the gradient (slope) of the function.
   float oxy = (OR - b) / m;     // This is the oxygen value.
 
   return (oxy);
